e8ce56c51af8a0115ee39fd76565eec9d8b9ffee
sudippal7778872 <105801008+sudippal7778872@users.noreply.github.com>
2023-12-13 19:53:49 +0530

    assets script model and vulnerability model
    assets script model and vulnerability model

--- otaa-server/src/model/asset.model.js
+++ otaa-server/src/model/asset.model.js
@@ -1,23 +1,163 @@
+// const mongoose = require("mongoose");
+
+// const vunerabilitySchema = new mongoose.Schema({
+//   Advisory_No: {
+//     type: String,
+//   },
+//   Advisory_Links: {
+//     type: [String],
+//   },
+//   Title: {
+//     type: String,
+//   },
+//   Manufacturer: {
+//     type: String,
+//   },
+//   Type: {
+//     type: String,
+//   },
+// });
+
+// const assetSchema = new mongoose.Schema({
+//   Vendor_ID: {
+//     type: String,
+//     default: "-",
+//   },
+//   Device_Type: {
+//     type: String,
+//     default: "-",
+//   },
+//   Product_Name: {
+//     type: String,
+//     default: "-",
+//   },
+//   Version: {
+//     type: String,
+//     default: "-",
+//   },
+//   Serial_Number: {
+//     type: String,
+//     default: "-",
+//   },
+//   IP: {
+//     type: String,
+//     default: "-",
+//   },
+//   Mac: {
+//     type: String,
+//     default: "-",
+//   },
+//   System_Name: {
+//     type: String,
+//     default: "-",
+//   },
+//   System_Description: {
+//     type: String,
+//     default: "-",
+//   },
+//   Module_Number: {
+//     type: String,
+//     default: "-",
+//   },
+//   CPU_Type: {
+//     type: String,
+//     default: "-",
+//   },
+//   Addtional_Component: {
+//     type: String,
+//     default: "-",
+//   },
+//   Vulnerabilitiy: {
+//     type: [vunerabilitySchema],
+//   },
+
+//   CreatedAt: {
+//     type: Date,
+//     default: Date.now(),
+//   },
+//   userId: {
+//     type: String,
+//     required: [true, "User id is required"],
+//   },
+//   UpdatedAt: {
+//     type: Date,
+//   },
+//   UpdatedBy: {
+//     type: String,
+//   },
+// });
+
+// const assetPlaylist = new mongoose.model("asset", assetSchema);
+
+// module.exports = assetPlaylist;
 const mongoose = require("mongoose");
 
-const assetSchema = new mongoose.Schema({
-  "Vendor ID": {
+const vulnerabilitySchema = new mongoose.Schema({
+  Advisory_No: {
     type: String,
     default: "-",
   },
-  "Device Type": {
+  Advisory_Link: {
+    type: [String],
+    default: "-",
+  },
+  Title: {
     type: String,
     default: "-",
   },
-  "Product Name": {
+  Manufacturer: {
     type: String,
     default: "-",
   },
-  "Version": {
+  Type: {
     type: String,
     default: "-",
   },
-  "Serial Number": {
+});
+
+const vulnerabilitySummerySchema = new mongoose.Schema({
+  Advisory_No: {
+    type: String,
+    default: "-",
+  },
+
+  Title: {
+    type: String,
+    default: "-",
+  },
+
+  Type: {
+    type: String,
+    default: "-",
+  },
+  Asset_MAC: {
+    type: String,
+    default: "-",
+  },
+  Asset_IP: {
+    type: String,
+    default: "-",
+  },
+});
+
+const assetSummerySchema = new mongoose.Schema({
+  Vendor_ID: {
+    type: String,
+    default: "-",
+  },
+  Device_Type: {
+    type: String,
+    default: "-",
+  },
+  Product_Name: {
+    type: String,
+    default: "-",
+  },
+  Version: {
+    type: String,
+    default: "-",
+  },
+  Serial_Number: {
     type: String,
     default: "-",
   },
@@ -29,25 +169,41 @@ const assetSchema = new mongoose.Schema({
     type: String,
     default: "-",
   },
-  "System Name": {
+  Vulnerability: {
+    type: [vulnerabilitySchema],
+  },
+  System_Name: {
     type: String,
     default: "-",
   },
-  "System Description": {
+  System_Description: {
     type: String,
     default: "-",
   },
-  "Module Number": {
+  Module_Number: {
     type: String,
     default: "-",
   },
-  "CPU Type": {
+  CPU_Type: {
     type: String,
     default: "-",
   },
-  "Addtional Component": {
+  Addtional_Component: {
     type: String,
     default: "-",
+  },
+});
+
+const assetSchema = new mongoose.Schema({
+  UserID: {
+    type: String,
+    required: [true, "User id is required"],
+  },
+  Asset_Summery: {
+    type: [assetSummerySchema],
+  },
+  Vulnerability_Summery: {
+    type: [vulnerabilitySummerySchema],
   },
   CreatedAt: {
     type: Date,
